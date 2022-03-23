82618bbfe1f5b5653e09377a2dc280fe8359f4e0
Tyler Pena <tylerpena@me.com>
2022-03-23 11:44:53 -0700

    added web config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import { Network } from "src/app/models/networks";
 
 let apiOverride: string;
 
-apiOverride = 'https://v1-dev-api.opdex.com/v1';
+// apiOverride = 'https://v1-dev-api.opdex.com/v1';
 // apiOverride = 'http://localhost:44391/v1';
 
 export const environment = {
