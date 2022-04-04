1bb0be3e22a833e6ba2edbd330d0563ebbc4322e
Tyler Pena <tylerpena@me.com>
2022-04-04 14:43:13 -0700

    network banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
+import { Icons } from './models/icons';
+import { Network } from './models/networks';
+import { EnvironmentsService } from './services/utility/environments.service';
 import { Component} from '@angular/core';
+
 @Component({
   selector: 'opdex-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  network: Network;
+  icons = Icons;
+
+  constructor(private _env: EnvironmentsService) {
+    this.network = this._env.network;
+  }
+}
